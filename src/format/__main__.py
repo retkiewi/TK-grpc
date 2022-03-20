eed84ba7f01f6efd4a1fa331385f763839db7708
Tequilac <ptekielak@gmail.com>
2022-03-20 15:04:47 +0100

    Change to single file

--- src/format/__main__.py
+++ src/format/__main__.py
@@ -1,24 +1,14 @@
 import os
 
-from src.core.walker import DirWalker
-
 
 def main():
     pass
 
 
-def get_files_by_format(search_path: str, desired_formats: list[str], undesired_formats: list[str]) -> list[str]:
-    results = []
-
-    def check_file(filename: str):
-        _, ext = os.path.splitext(filename)
-        if ext not in undesired_formats:
-            if len(desired_formats) == 0 or ext in desired_formats:
-                results.append(filename)
-
-    dir_walker = DirWalker(search_path, check_file)
-    dir_walker.walk()
-    return results
+def check_for_formats(file_path: str, desired_formats: list[str], undesired_formats: list[str]) -> bool:
+    _, ext = os.path.splitext(file_path)
+    if ext not in undesired_formats:
+        return len(desired_formats) == 0 or ext in desired_formats
 
 
 if __name__ == '__main__':
