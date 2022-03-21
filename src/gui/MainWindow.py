610b3ecf32930c3af601e8928f30250f34b4a907
Dawid_Bialka <dbialka@student.agh.edu.pl>
2022-03-21 18:00:04 +0100

    TK22WTWG-8 small fix

--- src/gui/MainWindow.py
+++ src/gui/MainWindow.py
@@ -6,8 +6,8 @@ from src.gui.selectdirectorywidget import SelectDirectoryWidget
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        self.form_widget = SelectDirectoryWidget(self)
+        self.setCentralWidget(self.form_widget)
         self.setGeometry(300, 300, 600, 400)
-        self.setCentralWidget(SelectDirectoryWidget(self))
         self.setWindowTitle("Welcome")
         self.show()
--- src/gui/selectdirectorywidget.py
+++ src/gui/selectdirectorywidget.py
@@ -1,24 +1,20 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog
 
 
-class QStringList(object):
-    pass
-
-
 class SelectDirectoryWidget(QWidget):
     def __init__(self, parent=None):
         super(SelectDirectoryWidget, self).__init__(parent)
 
         layout = QVBoxLayout()
-        layout.setSpacing(0)
         self.btn = QPushButton("Select directory")
-        self.btn.clicked.connect(self.getfile)
+        self.btn.clicked.connect(self.get_directory)
         self.label = QLabel('Selected directory: ')
         layout.addWidget(self.btn)
         layout.addWidget(self.label)
         self.setLayout(layout)
 
-    def getfile(self):
-        dir_name = QFileDialog.getExistingDirectory(self, 'Select folder')
+    def get_directory(self):
+        dir_name = QFileDialog.getExistingDirectory(self, 'Select directory')
         self.label.setText('Selected directory: ' + dir_name)
+
 
