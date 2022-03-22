120f55c8d2b4f82b2b0de0af6845c992af3b4a20
Tequilac <ptekielak@gmail.com>
2022-03-22 22:46:22 +0100

    Review fixes

--- src/format/__main__.py
+++ src/format/__main__.py
@@ -5,10 +5,9 @@ def main():
     pass
 
 
-def check_for_formats(file_path: str, desired_formats: list[str], undesired_formats: list[str]) -> bool:
+def check_for_formats(file_path: str, desired_formats: list[str]) -> bool:
     _, ext = os.path.splitext(file_path)
-    if ext not in undesired_formats:
-        return len(desired_formats) == 0 or ext in desired_formats
+    return ext in desired_formats
 
 
 if __name__ == '__main__':
