2fbb29c2abcb342b5d04b43c4f3b98a04d0fc297
Krzysztof Jędrejasz <krzysjed@wp.pl>
2022-03-27 23:27:30 +0200

    A little stylistic change

--- src/body/face_detection.py
+++ src/body/face_detection.py
@@ -6,4 +6,4 @@ def face_detection(path_file: str) -> int:
     img = cv2.imread(path_file)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _, _, level_weights = face_cascade.detectMultiScale3(gray, 1.1, 6, outputRejectLevels=True)
-    return 100-1/max(level_weights) if len(level_weights) == 0 else 0
+    return 100-1/max(level_weights) if level_weights else 0
