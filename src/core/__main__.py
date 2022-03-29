8b6889f1869fc85515b0b8469457592db20f0b8b
Tequilac <58514571+tequilac@users.noreply.github.com>
2022-03-29 15:04:48 +0200

    Minor fix

--- src/core/__main__.py
+++ src/core/__main__.py
@@ -1,8 +1,18 @@
+import grpc
+import core_format_pb2_grpc
+import core_format_pb2
+
 
 def main():
-    # Start all components
-    pass    
+    # TODO Start all components
 
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = core_format_pb2_grpc.FormatStub(channel)
+    response = stub.check_format(core_format_pb2.Request(path='C:/Users/pteki/UM/UM2/head_pose_estimation/headPose.jpg', formats=['.jpg']))
+    print("Greeter client received: " + str(response.response))
+    response = stub.check_format(core_format_pb2.Request(path='you', formats=['.jpg']))
+    print("Greeter client received: " + str(response.response))
+    pass    
 
 
 if __name__ == '__main__':
--- src/format/__main__.py
+++ src/format/__main__.py
@@ -1,8 +1,30 @@
 import os
+import grpc
+import core_format_pb2
+import core_format_pb2_grpc
+import concurrent.futures
 
 
 def main():
-    pass
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=10))
+    core_format_pb2_grpc.add_FormatServicer_to_server(Format(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+class Format(core_format_pb2_grpc.Format):
+    def check_format(self,
+                     target,
+                     options=(),
+                     channel_credentials=None,
+                     call_credentials=None,
+                     insecure=False,
+                     compression=None,
+                     wait_for_ready=None,
+                     timeout=None,
+                     metadata=None):
+        return core_format_pb2.Response(response=check_for_formats(target.path, target.formats))
 
 
 def check_for_formats(file_path: str, desired_formats: list[str]) -> bool:
