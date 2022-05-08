6c09d6038356ea3c01f53bc4a8bab36d9c1d91c6
Krzysztof Jędrejasz <krzysjed@wp.pl>
2022-05-09 00:59:30 +0200

    Small Fix

--- src/body/load_image.py
+++ src/body/load_image.py
@@ -3,33 +3,24 @@ import os
 import requests
 import numpy as np
 from requests.exceptions import InvalidSchema
+from typing import Optional
 
 
-def img_from_path(path):
+def img_from_path(path: str) -> Optional[np.ndarray]:
     try:
         image = cv2.imread(os.path.normpath(path))
     except cv2.error:
         print(f"Error: No valid image path: {path}")
-        return -1
-    else:
-        if image is not None:
-            return image
-        else:
-            print(f"Error: Path {path} returned None!")
-            return -1
+        return None
+    return image
 
 
-def img_from_link(link):
+def img_from_link(link: str) -> Optional[np.ndarray]:
     try:
         resp = requests.get(link, stream=True).raw
         image = np.asarray(bytearray(resp.read()), dtype="uint8")
         image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     except (InvalidSchema, requests.exceptions.ConnectionError):
         print(f"Error: No valid image link: {link}")
-        return -1
-    else:
-        if image is not None:
-            return image
-        else:
-            print(f"Error: Link {link} returned None!")
-            return -1
+        return None
+    return image
