84a11340dcd84f1cd4dd0aa85d8ffeb3cb017c4a
przemq20 <lechowiczprzemyslaw@gmail.com>
2022-05-24 12:20:36 +0200

    Renamed linux scripts

---
+++ src/old-start.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+# To add component just append names to the components array
+components=(
+    animal
+    body
+    format
+    style
+)
+
+components_num=${#components[@]}
+shell_pids=()
+PYTHON3=$(which python3)
+
+
+on_exit () {
+    for PID in ${shell_pids[@]} ; do
+        /bin/kill $(ps -o pid= --ppid $PID)
+    done
+    
+    exit 0
+}
+trap on_exit SIGINT
+
+for i in `seq 1 $components_num` ;
+do
+    comp_name=${components[$(($i - 1))]}
+    echo "Running... ${comp_name}"
+
+    # change it to sth else to run differently
+    start_command="$PYTHON3 -m $comp_name 2>&1"
+
+    bash -c "$start_command | sed -e \"s/^/[$comp_name] /\"" 2> /dev/null &
+    shell_pids[$i]=$!
+    echo "Done"
+done
+echo "All started!"
+
+$PYTHON3 -m core
+
+on_exit
