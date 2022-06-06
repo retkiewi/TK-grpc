ce5281329628a1df59458a2e79bf00fc35ac6cdd
Marek Ślązak <mslazak99@gmail.com>
2022-06-06 20:10:06 +0200

    Fix size filter

--- Client/SizeFilter/SizeFilter.py
+++ Client/SizeFilter/SizeFilter.py
@@ -13,12 +13,14 @@ def get_disk_size(path):
 
 
 def get_pixel_size(path):
-    imagesize.get(path)
+    return imagesize.get(path)
 
 
 def get_cm_size(path):
     width, height = imagesize.get(path)
     widthDPI, heightDPI = imagesize.getDPI(path)
+    print(width, height)
+    print(widthDPI, heightDPI)
     return [width / widthDPI * INCH2CM, height / heightDPI * INCH2CM]
 
 
@@ -31,24 +33,26 @@ def get_metric(unit):
 
 def is_compliant(path, metric, comparator, reference):
     value = metric(path)
-    return any(map(comparator, value, reference))
+    ref = reference if isinstance(reference, list) else [float(reference)]
+    print(f'Checked({value}) has type: {type(value)}')
+    print(f'Reference({ref}) has type: {type(ref)}')
+    return any(map(comparator, value, ref))
 
 
 def process_request(body: str):
     body = json.loads(body)
     params = body["params"]
+    paths = body['paths']
 
-    paths = params['paths']
     metric = get_metric(params['unit'])
     threshold = float(params['threshold']) if 'threshold' in params else 0
     comparator = get_comparator(params['comparator'], threshold)
     reference = params[params['unit']]
 
-    return filter(lambda path: is_compliant(path, metric, comparator, reference), paths)
+    return [*filter(lambda path: is_compliant(path, metric, comparator, reference), paths)]
 
 
 def process_single(query):
-
     metric = get_metric(query.unit)
     comparator = get_comparator(query.comparator, query.threshold)
     reference = query.values
