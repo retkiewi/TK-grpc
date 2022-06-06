62b2e29f1cfda687cd3b226a73d0d5204b4891d8
Krzysztof <kczech123kczech123@gmail.com>
2022-06-06 22:43:23 +0200

    Dogs filter Consumer  + proto

--- Client/DogFilterConsumer.py
+++ Client/DogFilterConsumer.py
@@ -1,8 +1,11 @@
 import logging
 import traceback
-
+import sys
+from Client import GRPCQueryListener
+from core_dogs_pb2_grpc import Dogs, add_DogsServicer_to_server as add_Dogs
+from core_dogs_pb2 import DogsResponse
 from Logger.CustomLogFormatter import CustomLogFormatter
-from DogsFilter.DogsBreedFilter import process_request
+from DogsFilter.DogsBreedFilter import process_request, process_single
 from Client import RabbitMQQueryListener
 from Message import RabbitMQResponse
 
@@ -16,8 +19,14 @@ logger.addHandler(ch)
 SERVICE_NAME = "dog_service"
 QUEUE_CONFIG_NAME = 'dogs_breeds'
 
+class DogsGRPC(Dogs):
+    def get_result(self, target, *args, **kwargs):
+        logger.info(f'recieved request for path {target.path}')
+        res = process_single(target)
+        logger.info(f'{res}: {type(res)}')
+        return DogsResponse(return_value=res)
 
-if __name__ == '__main__':
+def setup_rmq():
     logger.info("Starting SimilarityConsumer")
     consumer = RabbitMQQueryListener(QUEUE_CONFIG_NAME)
     logger.info("SimilarityConsumer started successfully")
@@ -35,3 +44,13 @@ if __name__ == '__main__':
 
 
     consumer.listen(callback)
+
+def setup_grpc():
+    consumer = GRPCQueryListener()
+    consumer.listen(lambda server: add_Dogs(DogsGRPC(), server), QUEUE_CONFIG_NAME)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2 or sys.argv[1] == 'rmq':
+        setup_rmq()
+    else:
+        setup_grpc()
